2c8cbc21589348b559fd51ebda501db8f353248b
athallahannafis <athallahannafis@gmail.com>
2021-05-17 23:26:38 +0700

    Add database

---
+++ database.pl
@@ -0,0 +1,18 @@
+% Database mahasiswa
+mahasiswa(edward).
+mahasiswa(ama).
+mahasiswa(nafis).
+mahasiswa(fauzan).
+mahasiswa(ardo).
+mahasiswa(farras).
+mahasiswa(fauzan).
+mahasiswa(fadhil).
+
+% Database mata kuliah
+% matakuliah/3
+% /1 : nama matkul
+% /2 : nama dosen
+% /3 : matkul prasyarat
+
+matakuliah(gamedev, daya_adianto, [sda,ddp,matdis]).
+matakuliah(sistem_interaksi, harry_budi_santoso, []).

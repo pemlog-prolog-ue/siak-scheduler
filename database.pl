c3d8edd4c25f7c58a157713c66290e6b65b85526
Athallah Annafis <athallahannafis@gmail.com>
2021-05-20 14:33:47 +0700

    Update mahasiswa database

--- database.pl
+++ database.pl
@@ -1,12 +1,25 @@
 % Database mahasiswa
-mahasiswa(edward).
-mahasiswa(ama).
-mahasiswa(nafis).
-mahasiswa(fauzan).
-mahasiswa(ardo).
-mahasiswa(farras).
-mahasiswa(fauzan).
-mahasiswa(fadhil).
+mahasiswa(m_01, "Frank Dominguez").
+mahasiswa(m_02, "James Garza").
+mahasiswa(m_03, "William Santana").
+mahasiswa(m_04, "Thomas Hale").
+mahasiswa(m_05, "Joseph Tyler").
+mahasiswa(m_06, "Robert Mcdowell").
+mahasiswa(m_07, "Edward Olsen").
+mahasiswa(m_08, "George Riley").
+mahasiswa(m_09, "Charles Vaughn").
+mahasiswa(m_10, "John Hensley").
+mahasiswa(m_11, "Marie Crawford").
+mahasiswa(m_12, "Ruth Holt").
+mahasiswa(m_13, "Dorothy Wiley").
+mahasiswa(m_14, "Mary Glover").
+mahasiswa(m_15, "Margaret Sears").
+mahasiswa(m_16, "Anna Goodman").
+mahasiswa(m_17, "Mildred Bartlett").
+mahasiswa(m_18, "Alice Lara").
+mahasiswa(m_19, "Elizabeth Romero").
+mahasiswa(m_20, "Helen Rhodes").
+
 
 % Database mata kuliah
 % matakuliah/3

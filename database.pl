3be215a4d46004521ae82e558b301e353b56d6a1
Bunga Amalia Kurniawati <bungasokia@gmail.com>
2021-05-20 21:45:44 +0700

    [ADD] add courses data

--- database.pl
+++ database.pl
@@ -22,10 +22,52 @@ mahasiswa(m_20, "Helen Rhodes").
 
 
 % Database mata kuliah
-% matakuliah/3
-% /1 : nama matkul
-% /2 : nama dosen
-% /3 : matkul prasyarat
+% matakuliah/2
+% /1 : kode mata kuliah
+% /2 : nama mata kuliah
 
-matakuliah(gamedev, daya_adianto, [sda,ddp,matdis]).
-matakuliah(sistem_interaksi, harry_budi_santoso, []).
+mata_kuliah(mk_01, ‘Aljabar Linier’)
+mata_kuliah(mk_02, ‘Analisis Numerik’)
+mata_kuliah(mk_03, ‘Basis Data’)
+mata_kuliah(mk_04, ‘Dasar-Dasar Pemrograman 1’)
+mata_kuliah(mk_05, ‘Dasar-Dasar Pemrograman 2’)
+mata_kuliah(mk_06, ‘Desain dan Analisis Algoritma’)
+mata_kuliah(mk_07, ‘Grafika Komputer’)
+mata_kuliah(mk_08, ‘Jaringan Komputer’)
+mata_kuliah(mk_09, ‘Kalkulus 1’)
+mata_kuliah(mk_10, ‘Kalkulus 2’)
+mata_kuliah(mk_11, ‘Kerja Praktik’)
+mata_kuliah(mk_12, ‘Komputer dan Masyarakat’)
+mata_kuliah(mk_13, ‘Kriptografi dan Keamanan Informasi’)
+mata_kuliah(mk_14, ‘Layanan dan Aplikasi Web’)
+mata_kuliah(mk_15, ‘Logika Komputasional’)
+mata_kuliah(mk_16, ‘Matematika Diskret 2’)
+mata_kuliah(mk_17, ‘Matematika Diskret 1’)
+mata_kuliah(mk_18, ‘Metodologi Penelitian dan Penulisan Ilmiah’)
+mata_kuliah(mk_19, ‘MPK Olahraga Bola Basket’)
+mata_kuliah(mk_20, ‘MPK Olahraga Bola Voli’)
+mata_kuliah(mk_21, ‘MPK Olahraga Futsal’)
+mata_kuliah(mk_22, ‘MPK Olahraga Sepakbola’)
+mata_kuliah(mk_23, ‘MPK Olahraga Tenis’)
+mata_kuliah(mk_24, ‘MPK Olahraga Tenis Meja’)
+mata_kuliah(mk_25, ‘MPK Terintegrasi’)
+mata_kuliah(mk_26, ‘Pemelajaran Mesin’)
+mata_kuliah(mk_27, ‘Pemrograman Kompetitif’)
+mata_kuliah(mk_28, ‘Pemrograman Lanjut’)
+mata_kuliah(mk_29, ‘Pemrograman Logika’)
+mata_kuliah(mk_30, ‘Pemrograman Paralel’)
+mata_kuliah(mk_31, ‘Pengantar Organisasi Komputer’)
+mata_kuliah(mk_32, ‘Pengantar Sistem Digital’)
+mata_kuliah(mk_33, ‘Pengolahan Bahasa Manusia’)
+mata_kuliah(mk_34, ‘Perancangan dan Pemrograman Web’)
+mata_kuliah(mk_35, ‘Persamaan Diferensial’)
+mata_kuliah(mk_36, ‘Proyek Perangkat Lunak’)
+mata_kuliah(mk_37, ‘Robotika’)
+mata_kuliah(mk_38, ‘Sains Data’)
+mata_kuliah(mk_39, ‘Sistem Cerdas’)
+mata_kuliah(mk_40, ‘Sistem Interaksi’)
+mata_kuliah(mk_41, ‘Sistem Operasi’)
+mata_kuliah(mk_42, ‘Sistem Operasi untuk Sistem Informasi’)
+mata_kuliah(mk_43, ‘Statistika dan Probabilitas’)
+mata_kuliah(mk_44, ‘Struktur Data dan Algoritma’)
+mata_kuliah(mk_45, ‘Teori Bahasa & Automata’)

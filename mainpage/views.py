df1330289f1060c454e8e5a919813cf976cfa356
athallahannafis <athallahannafis@gmail.com>
2021-05-29 17:38:49 +0700

    New url when doing query

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from pyswip.prolog import Prolog
 
 # Create your views here.
 def index(request):
+  return render(request, "index.html")
+
+def query(request):
+  prolog = Prolog()
   if request.method == "POST":
     print("MASUK")
     preferensi_teman = request.POST.get("preferensi_teman")
@@ -12,6 +17,6 @@ def index(request):
       "preferensi_dosen": preferensi_dosen,
       "preferensi_minat": preferensi_minat
     }
-    print(request_data)
     return render(request, "index.html", {"request_data": request_data})
-  return render(request, "index.html")
+  else:
+    return render(request, "index.html")

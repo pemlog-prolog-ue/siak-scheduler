0555c18e994a1be3b3b75ded3a32273accbec70a
athallahannafis <athallahannafis@gmail.com>
2021-05-31 20:38:08 +0700

    Render friend choices

--- siak-scheduler-app/src/pages/landing.js
+++ siak-scheduler-app/src/pages/landing.js
@@ -18,10 +18,19 @@ export default class Landing extends React.Component {
       matakuliah_lulus: "",
       list_matakuliah_lulus: [],
       list_result: [],
+      // Generate choices
       list_dosen_choices:[],
-      chosenDosen: []
+      chosenDosen: [],
+      list_friend_choices: [],
+      chosenFriends: []
+    }
+    this.style = {
+      searchBox: {
+        fontSize: "2px"
+      }
     }
     this.handleDosenChoice = this.handleDosenChoice.bind(this);
+    this.handleFriendChoice = this.handleFriendChoice.bind(this);
   }
 
   dosenHandler = (e) => {
@@ -47,29 +56,47 @@ export default class Landing extends React.Component {
 
   handleDosenChoice(option) {
     this.setState({
-      chosenDosen: option
+      chosenDosen: option,
+    })
+  }
+
+  handleFriendChoice(option) {
+    this.setState({
+      chosenFriends: option
     })
   }
 
   generate_result = (session, program, query) => {
     const show = (ans) => {
       var query_result = session.format_answer(ans);
-      // console.log(query_result);
-      console.log(query_result)
       var functor = query.split("(")[0];
-
+      // console.log(query_result);
       // Render dosen choice
       if (functor === "dosen" && query_result !== "false.") {
         var id_dosen = query_result.split(" = ")[1];
         id_dosen = id_dosen.split(", ")[0]
         var nama_dosen = query_result.split(" = ")[2];
-        console.log(id_dosen);
-        console.log(nama_dosen);
         var json_object = {
           "id_dosen": id_dosen,
           "nama_dosen": nama_dosen
         }
         this.setState({list_dosen_choices: this.state.list_dosen_choices.concat(json_object)});
+      }
+
+      // Render mahasiswa (teman) choice
+      if (functor === "mahasiswa" && query_result !== "false.") {
+        console.log(query_result)
+        var id_mhs = query_result.split(" = ")[1];
+        id_mhs = id_mhs.split(", ")[0]
+        var nama_mhs = query_result.split(" = ")[2];
+        // console.log(id_mhs);
+        // console.log(nama_mhs)
+        var json_object = {
+          "id_mhs": id_mhs,
+          "nama_mhs": nama_mhs
+        }
+        console.log(json_object)
+        this.setState({list_friend_choices: this.state.list_friend_choices.concat(json_object)});
       }
     }
 
@@ -89,12 +116,22 @@ export default class Landing extends React.Component {
     var session = pl.create(1000);
     var program = program_pl + scheduler_pl;
     // console.log(program);
-    const query = "dosen(Id_dosen, Nama_dosen)."
+    const query = "dosen(ID, Nama)."
+    this.generate_result(session, program, query);
+  }
+
+  fetchMahasiswaChoices = () => {
+    var pl = require("tau-prolog")
+    var session = pl.create(1000);
+    var program = program_pl + scheduler_pl;
+    // console.log(program);
+    const query = "mahasiswa(ID, Nama)."
     this.generate_result(session, program, query);
   }
 
   componentDidMount = () => {
     this.fetchDosenChoices();
+    this.fetchMahasiswaChoices();
   }
   testrender = () => {
     return (
@@ -104,7 +141,7 @@ export default class Landing extends React.Component {
     )
   }
   render() {
-    // console.log(this.state.list_dosen_choices)
+    // console.log(this.state.list_friend_choices)
     // console.log(this.state.chosenDosen);
     return (
       <div className="main-container">
@@ -116,6 +153,7 @@ export default class Landing extends React.Component {
             <label>Preferensi dosen</label>
             <div>
               <Multiselect
+              style={this.style}
               onSelect={this.handleDosenChoice}
               onRemove={this.handleDosenChoice}
               options={this.state.list_dosen_choices}
@@ -126,18 +164,42 @@ export default class Landing extends React.Component {
           </div>
           <div className="field-container">
             <label>Preferensi teman</label>
-            <input type="text" value={this.state.preferensi_teman}
-            onChange={this.temanHandler}/>
+            <div>
+              <Multiselect
+              style={this.style}
+              onSelect={this.handleFriendChoice}
+              onRemove={this.handleFriendChoice}
+              options={this.state.list_friend_choices}
+              placeholder="Pilih ingin sekelas dengan siapa"
+              displayValue="nama_mhs"
+              />
+            </div>
           </div>
           <div className="field-container">
             <label>Preferensi minat</label>
-            <input type="text" value={this.state.preferensi_minat}
-            onChange={this.minatHandler}/>
+            <div>
+              <Multiselect
+              style={this.style}
+              onSelect={this.handleDosenChoice}
+              onRemove={this.handleDosenChoice}
+              options={this.state.list_dosen_choices}
+              placeholder="Pilih ingin mengambil minat apa"
+              displayValue="nama_dosen"
+              />
+            </div>
           </div>
           <div className="field-container">
             <label>Mata kuliah yang sudah lulus:</label>
-            <input type="text" value={this.state.matakuliah_lulus}
-            onChange={this.mataKuliahLulusHandler}/>
+            <div>
+              <Multiselect
+              style={this.style}
+              onSelect={this.handleDosenChoice}
+              onRemove={this.handleDosenChoice}
+              options={this.state.list_dosen_choices}
+              placeholder="Masukan mata kuliah yang sudah lulus"
+              displayValue="nama_dosen"
+              />
+            </div>
           </div>
           <button type="submit" >Submit</button>
         </form>

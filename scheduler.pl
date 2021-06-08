382800899008ba14ce48a4a96dfb3c66fdc38f3b
edwardpartogi <52000607+edwardpartogi@users.noreply.github.com>
2021-06-08 23:33:33 +0700

    [MODIFY] Added some documentations

--- scheduler.pl
+++ scheduler.pl
@@ -4,47 +4,43 @@
 :- dynamic semua_list_kelas_sudah_terpilih/1.
 :- dynamic info_list_kelas_sudah_terpilih/2.
 
-%  BUAT NAFIS
-:- json_object info_kelas(nama_kelas:atom, nama_dosen:list, sks:integer, jadwal:list).
-:- json_object info_list_kelas_sudah_terpilih(sks:integer, semua_jadwal:list(info_kelas/4)).
-
-list_info_kelas([], []) :- !.
+list_info_kelas([], []).
 list_info_kelas([KelasA | KelasLainnya], [InfoKelasA | InfoKelasLainnya]):-
+    % Cek identitas kelas
     kelas(KelasA, MataKuliah, KodeKelasA), mata_kuliah(MataKuliah, NamaMataKuliah, SKSMataKuliah),
-    atomic_list_concat([NamaMataKuliah, KodeKelasA], ' ', NamaKelasA),
+    % Nama Kelas, ex : "DDP 1 - A"
+    atomic_list_concat([NamaMataKuliah, KodeKelasA], ' - ', NamaKelasA),
 
+    % Kumpulan Nama Dosen untuk 1 kelas
     findall(NamaDosenKelasA,
         (dosen_kelas(KelasA, DosenKelasA), dosen(DosenKelasA, NamaDosenKelasA)),
     BagNamaDosenKelasA),
     
+    % Kumpulan Jadwal untuk 1 kelas
     findall([HariKelasA, JamMulaiKelasA, MenitMulaiKelasA, JamSelesaiKelasA, MenitSelesaiKelasA],
         jadwal_kelas(KelasA, HariKelasA, JamMulaiKelasA, MenitMulaiKelasA, JamSelesaiKelasA, MenitSelesaiKelasA),
     BagJadwalA),
     jadwal_to_string(BagJadwalA, JadwalAString),
 
+    % Menyimpan semua info ke satu functor info_kelas/4
     InfoKelasA = info_kelas(NamaKelasA, BagNamaDosenKelasA, SKSMataKuliah, JadwalAString),
     list_info_kelas(KelasLainnya, InfoKelasLainnya).
 
-jadwal_to_string([], []):- !.
+jadwal_to_string([], []).
 jadwal_to_string([[Hari, JamMulai, MenitMulai, JamSelesai, MenitSelesai] | JadwalLainnya], [Jadwal1String | JadwalStringLainnya]):-
     atomics_to_string([JamMulai, MenitMulai], '.', JamMenitMulai),
     atomics_to_string([JamSelesai, MenitSelesai], '.', JamMenitSelesai),
     Jadwal1String = [Hari, JamMenitMulai, JamMenitSelesai],
     jadwal_to_string(JadwalLainnya, JadwalStringLainnya).
     
-    
-% jadwal_sesuai(IDMahasiswa, NamaMahasiswa, BatasSKS, PrefDosen, PrefTeman, PrefMataKuliah, ListMataKuliahSudahLulus):-
+% ----------------------------------------------------------------------------------------------------
+
 jadwal_sesuai(BatasSKS, ListMataKuliahSudahLulus):-
     retractall(semua_list_kelas_sudah_terpilih(_)), assertz(semua_list_kelas_sudah_terpilih([])),
     retractall(info_list_kelas_sudah_terpilih(_,_)),
     mata_kuliah_bisa_diambil(ListMataKuliahSudahLulus, ListMataKuliahBisaDiambil),
     kelas_bisa_diambil(ListMataKuliahBisaDiambil, ListKelasBisaDiambil),
     semua_list_kelas_terpilih(BatasSKS, ListKelasBisaDiambil, ListKelasBisaDiambil).
-
-cek_list_kelas_tidak_bentrok([_]) :- !.
-cek_list_kelas_tidak_bentrok([KelasA | KelasSisa]) :-
-    cek_1_kelas_tidak_bentrok_dengan_list_kelas(KelasA, KelasSisa),
-    cek_list_kelas_tidak_bentrok(KelasSisa).
 
 cek_1_kelas_tidak_bentrok_dengan_list_kelas(_, []).
 cek_1_kelas_tidak_bentrok_dengan_list_kelas(KelasA, [KelasB | KelasSisa]) :-
@@ -54,17 +50,22 @@ cek_1_kelas_tidak_bentrok_dengan_list_kelas(KelasA, [KelasB | KelasSisa]) :-
 tidak_bentrok_kelas(KelasA, KelasB):-
     bentrok(KelasA, KelasB), !, false.
 tidak_bentrok_kelas(KelasA, KelasB):-
-    % Cek kelas dan jadwalnya
+    % Cek info kelas dan jadwalnya
     kelas(KelasA, MataKuliah1, _),
     kelas(KelasB, MataKuliah2, _),
+
+    % Syarat 1 : Mata Kuliah berbeda
     MataKuliah1 \== MataKuliah2,
+
+    % Cek info jadwal dari kelas A dan B
     bagof([HariKelasA, JamMulaiKelasA, MenitMulaiKelasA, JamSelesaiKelasA, MenitSelesaiKelasA],
         jadwal_kelas(KelasA, HariKelasA, JamMulaiKelasA, MenitMulaiKelasA, JamSelesaiKelasA, MenitSelesaiKelasA),
     BagJadwalA),
     bagof([HariKelasB, JamMulaiKelasB, MenitMulaiKelasB, JamSelesaiKelasB, MenitSelesaiKelasB],
         jadwal_kelas(KelasB, HariKelasB, JamMulaiKelasB, MenitMulaiKelasB, JamSelesaiKelasB, MenitSelesaiKelasB),
     BagJadwalB),
-    % Cek kelas tidak bentrok untuk seluruh jadwal
+
+    % Syarat 2 : seluruh jadwal kelas A tidak bentrok dengan seluruh jadwal kelas B
     tidak_bentrok_jadwal(BagJadwalA, BagJadwalB), !.
 tidak_bentrok_kelas(KelasA, KelasB):-
     assertz(bentrok(KelasA, KelasB)), false.
@@ -77,7 +78,7 @@ tidak_bentrok_jadwal([JadwalA1 | JadwalASisa], BagJadwalB):-
     % Rekursif cek ke semua JadwalA
     tidak_bentrok_jadwal(JadwalASisa, BagJadwalB).
 
-cek_1_jadwal(_, []) :- !.
+cek_1_jadwal(_, []).
 cek_1_jadwal([HariKelasA, JamMulaiKelasA, MenitMulaiKelasA, JamSelesaiKelasA, MenitSelesaiKelasA],
     [[HariKelasB, JamMulaiKelasB, MenitMulaiKelasB, JamSelesaiKelasB, MenitSelesaiKelasB] | JadwalBSisa]):-
     % Cek bentrok di hari yang sama
@@ -91,7 +92,6 @@ cek_1_jadwal([HariKelasA, JamMulaiKelasA, MenitMulaiKelasA, JamSelesaiKelasA, Me
     % Rekursif cek ke semua JadwalB
     cek_1_jadwal([HariKelasA, JamMulaiKelasA, MenitMulaiKelasA, JamSelesaiKelasA, MenitSelesaiKelasA],
         JadwalBSisa).
-
 cek_1_jadwal(JadwalA, [_ | JadwalBSisa]):-
     cek_1_jadwal(JadwalA, JadwalBSisa).
 
@@ -103,22 +103,24 @@ memenuhi_prasyarat(MataKuliah, ListMataKuliahSudahLulus) :-
     prasyarat(MataKuliah, ListPrasyaratMataKuliah),
     subset(ListPrasyaratMataKuliah, ListMataKuliahSudahLulus).
 
+% Memeriksa mata kuliah yang memenuhi syarat : memenuhi Prasyarat Mata Kuliah dan Belum pernah lulus
 mata_kuliah_bisa_diambil(ListMataKuliahSudahLulus, ListMataKuliahBisaDiambil) :-
     bagof(IDMataKuliah,
         (
-            memenuhi_prasyarat(IDMataKuliah, ListMataKuliahSudahLulus),
-            \+member(IDMataKuliah, ListMataKuliahSudahLulus)
+            memenuhi_prasyarat(IDMataKuliah, ListMataKuliahSudahLulus), % Mata kuliah yang prasyaratnya terpenuhi
+            \+member(IDMataKuliah, ListMataKuliahSudahLulus)            % Mata kuliah yang belum lulus
         ),
         ListMataKuliahBisaDiambil).
 
+% Mengambil semua kelas dari mata kuliah yang diberikan
 kelas_bisa_diambil([], []).
 kelas_bisa_diambil([MataKuliahBisaDiambilA | MataKuliahBisaDiambilSisa], ListKelasBisaDiambil) :-
     bagof(IDKelas, NamaKelas^(kelas(IDKelas, MataKuliahBisaDiambilA, NamaKelas)), ListKelasBisaDiambilA),
     append(ListKelasBisaDiambilA, ListKelasBisaDiambilSisa, ListKelasBisaDiambil),
     kelas_bisa_diambil(MataKuliahBisaDiambilSisa, ListKelasBisaDiambilSisa).
 
-
-pernah_terpilih(_, []) :- !, false.
+% Memeriksa solusi sudah pernah terpilih, walaupun berbeda urutan
+pernah_terpilih(_, []) :- false.
 pernah_terpilih(ListKelas, [ListKelasPernahTerpilih | _]):-
     msort(ListKelas, ListKelasTerurut), msort(ListKelasPernahTerpilih, ListKelasTerurut), !.
 pernah_terpilih(ListKelas, [_| ListKelasPernahTerpilihLainnya]):-
@@ -126,35 +128,61 @@ pernah_terpilih(ListKelas, [_| ListKelasPernahTerpilihLainnya]):-
 
 semua_list_kelas_terpilih(_, [_|_], []) :- !.
 semua_list_kelas_terpilih(BatasSKS, ListKelasBisaDiambil, [_|ListTemporary]):-
+    % Cari 1 solusi susunan kelas yang memenuhi
     kelas_sesuai_sks(BatasSKS, ListKelasBisaDiambil, TotalSKS, ListKelasTerpilih, 0, []),
+
+    % Memeriksa apakah solusi baru sudah pernah terpilih
     semua_list_kelas_sudah_terpilih(SemuaListKelasPernahTerpilih),
     \+pernah_terpilih(ListKelasTerpilih, SemuaListKelasPernahTerpilih), !,
 
+    % Simpan ke kumpulan solusi baru di dynamic semua_list_kelas_sudah_terpilih/1
     append(SemuaListKelasPernahTerpilih, [ListKelasTerpilih], SemuaListKelasTerpilihBaru),
     retractall(semua_list_kelas_sudah_terpilih(_)), assertz(semua_list_kelas_sudah_terpilih(SemuaListKelasTerpilihBaru)),
+
+    % Menyimpan info dari solusi baru memakai dynamic info_list_kelas_sudah_terpilih/2
     list_info_kelas(ListKelasTerpilih, ListKelasTerpilih_Info),
     assertz(info_list_kelas_sudah_terpilih(TotalSKS, ListKelasTerpilih_Info)),
 
+    % Mengganti urutan ListKelasBisaDiambil untuk mencari kemungkinan solusi baru
     ListKelasBisaDiambil = [KelasA | SisaListKelasBisaDiambil],
     append(SisaListKelasBisaDiambil, [KelasA], ListKelasBisaDiambilBaru),
     semua_list_kelas_terpilih(BatasSKS, ListKelasBisaDiambilBaru, ListTemporary).
 semua_list_kelas_terpilih(BatasSKS, [KelasA | SisaListKelasBisaDiambil], [_|ListTemporary]):-
+    % Mengganti urutan ListKelasBisaDiambil untuk mencari kemungkinan solusi baru
     append(SisaListKelasBisaDiambil, [KelasA], ListKelasBisaDiambilBaru),
     semua_list_kelas_terpilih(BatasSKS, ListKelasBisaDiambilBaru, ListTemporary).
 
-kelas_sesuai_sks(_, [], 0, [], _, _) :- !.
-kelas_sesuai_sks(BatasSKS, ListKelasBisaDiambil, TotalSKSDiambil, KelasMemenuhi, SKSTemporary, ListTemporary):-
-    member(KelasBisaDiambilA, ListKelasBisaDiambil),
+
+% Untuk mendapatkan susunan kelas-kelas yang mungkin diambil, selama tidak saling bentrok dan tidak melewati batas SKS yang diberikan
+% Rule dijalankan secara rekursif, dengan memperhatikan 3 kasus :
+%     1. Tidak ada lagi Kelas yang dijadikan pilihan (Base Case)
+%     2. Jika masih ada Kelas yang menjadi pilihan, dan
+%         - SKS kelas tersebut tidak melewati batas SKS,
+%         - kelas tersebut tidak menimbulkan jadwal bentrok.
+%     3. Jika Kelas yang menjadi pilihan tidak memenuhi syarat pada kasus 2
+
+kelas_sesuai_sks(_, [], 0, [], _, _).
+kelas_sesuai_sks(BatasSKS, [KelasBisaDiambilA | SisaKelasBisaDiambil], TotalSKSDiambil, KelasMemenuhi, SKSTemporary, ListTemporary):-
+    % Ambil info Mata Kuliah
     kelas(KelasBisaDiambilA, MataKuliah, _),
     mata_kuliah(MataKuliah, _, SKSMataKuliah),
+    
+    % Periksa SKS dan Bentrok
     BatasSKS >= SKSTemporary+SKSMataKuliah,
     cek_1_kelas_tidak_bentrok_dengan_list_kelas(KelasBisaDiambilA, ListTemporary), !,
 
-    append([KelasBisaDiambilA], KelasMemenuhiLainnya, KelasMemenuhi),
-    append([KelasBisaDiambilA], ListTemporary, ListTemporaryBerikutnya),
+    % Menambahkan SKS Mata Kuliah yang memenuhi ke akumulator SKSTemporaryBerikutnya
     SKSTemporaryBerikutnya is SKSTemporary+SKSMataKuliah,
-    delete(ListKelasBisaDiambil, KelasBisaDiambilA, SisaKelasBisaDiambil),
+
+    % Menambahkan Kelas yang memenuhi ke akumulator KelasTemporaryBerikutnya
+    append([KelasBisaDiambilA], ListTemporary, ListTemporaryBerikutnya),
+
+    % Memisahkan Kelas yang memenuhi dengan KelasMemenuhiLainnya
+    append([KelasBisaDiambilA], KelasMemenuhiLainnya, KelasMemenuhi),
+
     kelas_sesuai_sks(BatasSKS, SisaKelasBisaDiambil, SKSKelasMemenuhiLainnya, KelasMemenuhiLainnya, SKSTemporaryBerikutnya, ListTemporaryBerikutnya),
+    
+    % Memisahkan SKS Mata Kuliah yang memenuhi dengan SKSKelasMemenuhiLainnya
     TotalSKSDiambil is SKSKelasMemenuhiLainnya+SKSMataKuliah.
 kelas_sesuai_sks(BatasSKS, [_ | SisaKelasBisaDiambil], TotalSKSDiambil, KelasMemenuhi, SKSTemporary, ListTemporary):-
     kelas_sesuai_sks(BatasSKS, SisaKelasBisaDiambil, TotalSKSDiambil, KelasMemenuhi, SKSTemporary, ListTemporary).

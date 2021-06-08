32a52c1bfad02a56e22524370cc92488903ea0f1
athallahannafis <athallahannafis@gmail.com>
2021-06-09 01:34:18 +0700

    Implement rendering with pagination

--- siak-scheduler-app/src/pages/landing.js
+++ siak-scheduler-app/src/pages/landing.js
@@ -1,10 +1,8 @@
 import React, {useState} from "react";
 import "./landing.css";
 import axios from "axios";
-import { program_pl } from "./database";
-import { scheduler_pl } from "./scheduler_pl";
 import {Multiselect} from "multiselect-react-dropdown";
-import { getAllMatkul, postQuery } from "./siak_api";
+import { getAllMatkul } from "./siak_api";
 
 
 export default class Landing extends React.Component {
@@ -34,7 +32,9 @@ export default class Landing extends React.Component {
       query: {
         graduated: [],
         sks: 0,
-      }
+      },
+      list_jadwal: [],
+      page: 1
     }
     this.style = {
       searchBox: {
@@ -65,6 +65,13 @@ export default class Landing extends React.Component {
 
   sksHandler = (e) => {
     this.setState({jumlah_sks: e.target.value})
+  }
+
+  nextHandler = (e, length) => {
+    this.setState({
+      page: this.state.page+1
+    });
+    e.preventDefault();
   }
 
   submitHandler = (e) => {
@@ -121,10 +128,15 @@ export default class Landing extends React.Component {
   fetchPostQuery = (json_data) => {
     const url = "http://localhost:8000/api/query";
     var query_data = json_data;
+
     console.log(query_data);
     axios.post(url, query_data)
     .then( response => {
-      console.log(response)
+      this.setState({
+        list_jadwal: response.data
+      }, () => {
+        console.log(this.state.list_jadwal);
+      })
     })
   }
 
@@ -139,43 +151,153 @@ export default class Landing extends React.Component {
     )
   }
   render() {
-    // console.log(this.state.list_friend_choices)
-    // console.log(this.state.chosenDosen);
-    // this.fetchPostQuery();
+    let index = 0;
+    const data_render = this.state.list_jadwal.map((item) => {
+      var key_id = index;
+      var list = item.info_satu_solusi;
+      // Fetch current nama_kelas
+      var temp_nama_kelas = [];
+      var fetch_nama_kelas = () => {
+        for (let i = 0; i < list.length; i++) {
+          temp_nama_kelas.push(list[i].nama_kelas);
+        }
+      }
+      fetch_nama_kelas();
+      index++;
+      return (
+        <div style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems:"center",
+          borderWidth:3,
+          borderStyle: "solid",
+          borderColor: "#8aebf2",
+          backgroundColor: "#C1DFE1",
+          padding: "3%",
+          borderRadius: 15,
+          marginBottom: 20,
+          maxWidth:700,
+        }} key={key_id}>
+          {/* SKS */}
+          <h2>Jadwal ke-{index} (Total SKS: {item.sks})</h2>
+          {/* Matkul */}
+          <div style={{
+            display:"flex",
+            flexDirection:"row",
+            justifyContent:"center",
+            flexWrap:"wrap"
+          }}>
+            {temp_nama_kelas.map((value_matkul, idx) => {
+              return (
+                <div style={{
+                  backgroundColor:"white",
+                  borderRadius: 20,
+                  margin:10,
+                  padding: 10,
+                  maxWidth: 200
+                }}>
+                  <h4>{value_matkul} ({item.info_satu_solusi[idx].sks} sks)</h4>
+                  Dosen:
+                  <ul>
+                    {item.info_satu_solusi[idx].nama_dosen.map((value_dosen,index) => {
+                      return (
+                        <>
+                          <li>
+                          {value_dosen}
+                          </li>
+                        </>
+                      )
+                    })}
+                  </ul>
+                  Jadwal:
+                  <ul>
+                    {item.info_satu_solusi[idx].jadwal.map((value_jadwal, index) => {
+                      return (
+                        <>
+                          <li>
+                            <p>
+                            {value_jadwal[0]} {value_jadwal[1]} {value_jadwal[2]} 
+                            </p>
+                          </li>
+                        </>
+                      )
+                    })}
+                  </ul>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      );
+    })
+    
+    const pagination_button = () => {
+      if (data_render.length !=0) {
+        return (
+          <div style={{
+            display:"flex", flexDirection:"row", justifyContent: "center",
+            margin:10
+          }}>
+            <button onClick={(e) => {
+              if(this.state.page < 2) {
+                this.setState({page: 1})
+              } else {
+                this.setState({page: this.state.page-1})
+              }
+            }}>
+              Prev
+            </button>
+            <p style={{marginLeft: 10, marginRight: 10}}>
+              {this.state.page} / {data_render.length}
+            </p>
+            <button onClick={(e) => {
+              if(this.state.page > data_render.length-1) {
+                this.setState({page: data_render.length})
+              } else {
+                this.setState({page: this.state.page+1})
+              }
+            }}>Next</button>
+          </div>
+        )
+      }
+    }
+
     return (
       <div className="main-container">
-        <h2>
-          Cari jadwal Siak
-        </h2>
-        <form className="form-container" onSubmit={this.submitHandler}>
-          <div className="field-container">
-            <label>Mata kuliah yang sudah lulus:</label>
-            <div>
-              <Multiselect
-              showCheckbox={true}
-              style={this.style}
-              onSelect={this.handleGraduatedChoice}
-              onRemove={this.handleGraduatedChoice}
-              options={this.state.list_matkul}
-              placeholder="Pilih mata kuliah"
-              displayValue="nama_matkul"
-              />
+        <div style={{marginTop:100}}>
+          <h2>
+            Cari jadwal Siak
+          </h2>
+          <form className="form-container" onSubmit={this.submitHandler}>
+            <div className="field-container">
+              <label>Mata kuliah yang sudah lulus:</label>
+              <div>
+                <Multiselect
+                showCheckbox={true}
+                style={this.style}
+                onSelect={this.handleGraduatedChoice}
+                onRemove={this.handleGraduatedChoice}
+                options={this.state.list_matkul}
+                placeholder="Pilih mata kuliah"
+                displayValue="nama_matkul"
+                closeOnSelect={false}
+                />
+              </div>
             </div>
-          </div>
-          <div className="field-container">
-            <label>SKS maksimal:</label>
-            <input
-            type="text"
-            value={this.state.jumlah_sks}
-            pattern="[0-9]*"
-            onChange={this.sksHandler}/>
-          </div>
-          <button type="submit" >Submit</button>
-        </form>
-        <div>
-          [{this.state.query.graduated}]
-          <br></br>
-          {this.state.query.sks}
+            <div className="field-container">
+              <label>SKS maksimal:</label>
+              <input
+              type="text"
+              value={this.state.jumlah_sks}
+              pattern="[0-9]*"
+              onChange={this.sksHandler}/>
+            </div>
+            <button type="submit" >Submit</button>
+          </form>
+
+          {pagination_button()}
+            {data_render[this.state.page-1]}
+          {pagination_button()}
         </div>
       </div>
     );
